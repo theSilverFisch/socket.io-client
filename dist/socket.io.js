3d07b001076ea8695cea7c2111815c356ce355a4
Kirk MacPhee <kirk@goinstant.com>
2012-11-28 16:27:40 -0400

    update version

--- dist/socket.io.js
+++ dist/socket.io.js
@@ -25,7 +25,7 @@ var io = ('undefined' === typeof module ? {} : module.exports);
    * @api public
    */
 
-  io.version = '0.9.11';
+  io.version = '0.9.11.goinstant';
 
   /**
    * Protocol implemented.
